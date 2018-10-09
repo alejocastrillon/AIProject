44edf4135e5e488b79c650025600dce79479ba4d
alejandro.castrillon <alejandro.castrillon@utp.edu.co>
2018-10-09 17:43:24 -0500

    Funciona el Speech Recognition

--- project.py
+++ project.py
@@ -1,14 +1,11 @@
 import speech_recognition as sr
+import pygame
 
 # obtain audio from the microphone
-
-'''miclist=sr.Microphone.list_microphone_names()
-for x in miclist:
-    print(x)'''
     
 r = sr.Recognizer()
 with sr.Microphone(device_index = 0) as source:
     r.adjust_for_ambient_noise(source)
-    print("Say something!")
+    print("Di alguito!")
     audio = r.listen(source)
 print(r.recognize_google(audio))
