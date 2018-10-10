ef89db651593747c13bd75c0cf611cc4d3536070
Alejandro Castrillon <jairocastrillon@misena.edu.co>
2018-10-09 23:31:50 -0500

    Subida parcial de puntos de la Ruta 1, quedé en el barrio El Dorado

---
+++ Data Structure Model/DATAROUNDPOINTS.sql
@@ -0,0 +1,57 @@
+/*
+-- Query: SELECT * FROM routes_city.points_route
+LIMIT 0, 1000
+
+-- Date: 2018-10-09 23:30
+*/
+INSERT INTO `points_route` (`idpoints_route`,`latitude`,`longitude`,`route_idroute`) VALUES (1,'4.812073','-75.661263',1);
+INSERT INTO `points_route` (`idpoints_route`,`latitude`,`longitude`,`route_idroute`) VALUES (2,'4.811540','-75.660998',1);
+INSERT INTO `points_route` (`idpoints_route`,`latitude`,`longitude`,`route_idroute`) VALUES (3,'4.810594','-75.662886',1);
+INSERT INTO `points_route` (`idpoints_route`,`latitude`,`longitude`,`route_idroute`) VALUES (4,'4.812904','-75.664077',1);
+INSERT INTO `points_route` (`idpoints_route`,`latitude`,`longitude`,`route_idroute`) VALUES (5,'4.813574','-75.661832',1);
+INSERT INTO `points_route` (`idpoints_route`,`latitude`,`longitude`,`route_idroute`) VALUES (6,'4.812337','-75.661085',1);
+INSERT INTO `points_route` (`idpoints_route`,`latitude`,`longitude`,`route_idroute`) VALUES (7,'4.812423','-75.660890',1);
+INSERT INTO `points_route` (`idpoints_route`,`latitude`,`longitude`,`route_idroute`) VALUES (8,'4.811943','-75.660634',1);
+INSERT INTO `points_route` (`idpoints_route`,`latitude`,`longitude`,`route_idroute`) VALUES (9,'4.813210','-75.658237',1);
+INSERT INTO `points_route` (`idpoints_route`,`latitude`,`longitude`,`route_idroute`) VALUES (10,'4.813648','-75.658114',1);
+INSERT INTO `points_route` (`idpoints_route`,`latitude`,`longitude`,`route_idroute`) VALUES (11,'4.814993','-75.659046',1);
+INSERT INTO `points_route` (`idpoints_route`,`latitude`,`longitude`,`route_idroute`) VALUES (12,'4.818403','-75.660795',1);
+INSERT INTO `points_route` (`idpoints_route`,`latitude`,`longitude`,`route_idroute`) VALUES (13,'4.826972','-75.674658',1);
+INSERT INTO `points_route` (`idpoints_route`,`latitude`,`longitude`,`route_idroute`) VALUES (14,'4.824030','-75.677483',1);
+INSERT INTO `points_route` (`idpoints_route`,`latitude`,`longitude`,`route_idroute`) VALUES (15,'4.822820','-75.676321',1);
+INSERT INTO `points_route` (`idpoints_route`,`latitude`,`longitude`,`route_idroute`) VALUES (16,'4.824656','-75.678282',1);
+INSERT INTO `points_route` (`idpoints_route`,`latitude`,`longitude`,`route_idroute`) VALUES (17,'4.824900','-75.679880',1);
+INSERT INTO `points_route` (`idpoints_route`,`latitude`,`longitude`,`route_idroute`) VALUES (18,'4.823006','-75.681140',1);
+INSERT INTO `points_route` (`idpoints_route`,`latitude`,`longitude`,`route_idroute`) VALUES (19,'4.823293','-75.683404',1);
+INSERT INTO `points_route` (`idpoints_route`,`latitude`,`longitude`,`route_idroute`) VALUES (20,'4.819006','-75.684839',1);
+INSERT INTO `points_route` (`idpoints_route`,`latitude`,`longitude`,`route_idroute`) VALUES (21,'4.816570','-75.682362',1);
+INSERT INTO `points_route` (`idpoints_route`,`latitude`,`longitude`,`route_idroute`) VALUES (22,'4.814697','-75.683227',1);
+INSERT INTO `points_route` (`idpoints_route`,`latitude`,`longitude`,`route_idroute`) VALUES (23,'4.817357','-75.685672',1);
+INSERT INTO `points_route` (`idpoints_route`,`latitude`,`longitude`,`route_idroute`) VALUES (24,'4.818006','-75.687584',1);
+INSERT INTO `points_route` (`idpoints_route`,`latitude`,`longitude`,`route_idroute`) VALUES (25,'4.818664','-75.688601',1);
+INSERT INTO `points_route` (`idpoints_route`,`latitude`,`longitude`,`route_idroute`) VALUES (26,'4.819138','-75.691119',1);
+INSERT INTO `points_route` (`idpoints_route`,`latitude`,`longitude`,`route_idroute`) VALUES (27,'4.820325','-75.692718',1);
+INSERT INTO `points_route` (`idpoints_route`,`latitude`,`longitude`,`route_idroute`) VALUES (28,'4.819929','-75.694810',1);
+INSERT INTO `points_route` (`idpoints_route`,`latitude`,`longitude`,`route_idroute`) VALUES (29,'4.821597','-75.700518',1);
+INSERT INTO `points_route` (`idpoints_route`,`latitude`,`longitude`,`route_idroute`) VALUES (30,'4.821952','-75.706640',1);
+INSERT INTO `points_route` (`idpoints_route`,`latitude`,`longitude`,`route_idroute`) VALUES (31,'4.821695','-75.709086',1);
+INSERT INTO `points_route` (`idpoints_route`,`latitude`,`longitude`,`route_idroute`) VALUES (32,'4.820989','-75.710202',1);
+INSERT INTO `points_route` (`idpoints_route`,`latitude`,`longitude`,`route_idroute`) VALUES (33,'4.819065','-75.711650',1);
+INSERT INTO `points_route` (`idpoints_route`,`latitude`,`longitude`,`route_idroute`) VALUES (34,'4.817076','-75.717497',1);
+INSERT INTO `points_route` (`idpoints_route`,`latitude`,`longitude`,`route_idroute`) VALUES (35,'4.816467','-75.716950',1);
+INSERT INTO `points_route` (`idpoints_route`,`latitude`,`longitude`,`route_idroute`) VALUES (36,'4.816136','-75.716725',1);
+INSERT INTO `points_route` (`idpoints_route`,`latitude`,`longitude`,`route_idroute`) VALUES (37,'4.815794','-75.718152',1);
+INSERT INTO `points_route` (`idpoints_route`,`latitude`,`longitude`,`route_idroute`) VALUES (38,'4.815644','-75.720904',1);
+INSERT INTO `points_route` (`idpoints_route`,`latitude`,`longitude`,`route_idroute`) VALUES (39,'4.815286','-75.720889',1);
+INSERT INTO `points_route` (`idpoints_route`,`latitude`,`longitude`,`route_idroute`) VALUES (40,'4.815168','-75.718926',1);
+INSERT INTO `points_route` (`idpoints_route`,`latitude`,`longitude`,`route_idroute`) VALUES (41,'4.814617','-75.718974',1);
+INSERT INTO `points_route` (`idpoints_route`,`latitude`,`longitude`,`route_idroute`) VALUES (42,'4.813827','-75.717225',1);
+INSERT INTO `points_route` (`idpoints_route`,`latitude`,`longitude`,`route_idroute`) VALUES (43,'4.812443','-75.717013',1);
+INSERT INTO `points_route` (`idpoints_route`,`latitude`,`longitude`,`route_idroute`) VALUES (44,'4.809573','-75.713343',1);
+INSERT INTO `points_route` (`idpoints_route`,`latitude`,`longitude`,`route_idroute`) VALUES (45,'4.807996','-75.710975',1);
+INSERT INTO `points_route` (`idpoints_route`,`latitude`,`longitude`,`route_idroute`) VALUES (46,'4.805527','-75.715592',1);
+INSERT INTO `points_route` (`idpoints_route`,`latitude`,`longitude`,`route_idroute`) VALUES (47,'4.805653','-75.716690',1);
+INSERT INTO `points_route` (`idpoints_route`,`latitude`,`longitude`,`route_idroute`) VALUES (48,'4.804337','-75.716717',1);
+INSERT INTO `points_route` (`idpoints_route`,`latitude`,`longitude`,`route_idroute`) VALUES (49,'4.803450','-75.719034',1);
+INSERT INTO `points_route` (`idpoints_route`,`latitude`,`longitude`,`route_idroute`) VALUES (50,'4.802744','-75.718422',1);
+INSERT INTO `points_route` (`idpoints_route`,`latitude`,`longitude`,`route_idroute`) VALUES (51,'4.801461','-75.718701',1);
